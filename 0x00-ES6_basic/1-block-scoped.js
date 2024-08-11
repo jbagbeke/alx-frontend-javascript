59b2d376005539b5aec080ad8c49030512a7b0d5
Joshua Bubune Agbeke <joshuabubune40@gmail.com>
2024-08-11 02:11:56 +0000

    0x00. ES6 Basics Task 1

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+  
+  if (trueOrFalse) {
+    let task = true;
+    let task2 = false;
+    console.log(task, task2);
+  }
+  
+  return [task, task2];
+}
