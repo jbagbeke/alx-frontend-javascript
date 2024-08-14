3a1bb25bdf16ee8d3585effc0b55f77446418823
Joshua Bubune Agbeke <joshuabubune40@gmail.com>
2024-08-14 00:26:33 +0000

    0x00. ES6 Basics Mandatory Task 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
   let i = 0;
   for (const value of array) {
-    array[i++] = appendString + value;
+    array[i] = appendString + value;
+    i += 1;
   }
   return array;
 }
