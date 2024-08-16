8ee5cb97b710da3f8ab091b9227362be4fcadae2
Joshua Bubune Agbeke <joshuabubune40@gmail.com>
2024-08-16 17:13:40 +0000

    0x01. ES6 Promises Mandatory Task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -2,9 +2,8 @@ export default function getResponseFromAPI (success) {
   if (success) {
     return Promise.resolve({
       status: 200,
-      body: 'success'
+      body: 'Success'
     })
-  } else {
-      return Promise.reject(Error('The fake API is not working currently'))
   }
+  return Promise.reject(new Error('The fake API is not working currently'))
 }
