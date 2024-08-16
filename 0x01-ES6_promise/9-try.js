352ac252721a8341ce63950ce0728330aa60454b
Joshua Bubune Agbeke <joshuabubune40@gmail.com>
2024-08-16 18:48:01 +0000

    0x01. ES6 Promises Mandatory Task 9

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,14 @@
+export default function guardrail (mathFunction) {
+  const queue = []
+
+  try {
+    const val = mathFunction()
+    queue.push(val)
+  } catch (err) {
+    queue.push(`${err}`)
+  } finally {
+    queue.push('Guardrail was processed')
+  }
+
+  return queue
+}
