0ac9415fdbe9a976a4af9bd984020ffa74aa0720
Joshua Bubune Agbeke <joshuabubune40@gmail.com>
2024-08-16 16:03:59 +0000

    0x00. ES6 Basics Advanced Task 14

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,9 +1,9 @@
 export default function createIteratorObject (report) {
-  const dep_emps = Object.values(report.allEmployees).flat()
+  const dep_emps = Object.values(report.allEmployees).flat();
   return {
     [Symbol.iterator]: function* () {
       for (let x of dep_emps) {
-        yield x
+        yield x;
       }
     }
   }
