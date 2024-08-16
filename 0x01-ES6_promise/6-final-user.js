1a13f74cafc6dcf90ec41a47479ccdfb2116d3a0
Joshua Bubune Agbeke <joshuabubune40@gmail.com>
2024-08-16 18:27:23 +0000

    0x01. ES6 Promises Mandatory Task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,5 +5,18 @@ export default function handleProfileSignup (firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName)
-  ])
+  ]).then((elements) => {
+      const arr = []
+      for (let elm of elements) {
+        let obj = {}
+        if (elm.status === 'rejected') {
+          obj.value = `${elm.reason}`
+        } else {
+          obj.value = elm.value
+        }
+        obj.status = elm.status
+        arr.push(obj)
+    }
+    return arr
+  })
 }
