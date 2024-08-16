577a9bb07c1b532b57b131861d7152675f1bc791
Joshua Bubune Agbeke <joshuabubune40@gmail.com>
2024-08-16 17:44:25 +0000

    0x01. ES6 Promises Mandatory Task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ export default function handleProfileSignup () {
   return Promise.all([uploadPhoto(), createUser()])
     .then(values => {
       const [{ body }, { firstName, lastName }] = [values[0], values[1]]
-      console.log(body, firstName, lastName)
+      console.log(`${body} ${firstName} ${lastName}`)
     })
     .catch(err => console.log('Signup system offline'))
 }
