f86930b2295dbcaf585e06db002a222079f6eba6
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-08-23 12:51:53 +0000

    0x03. ES6 data manipulation Mandatory Task 7

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,3 +1,8 @@
 export default function hasValuesFromArray (set, arr) {
-  return set.isSupersetOf(new Set(arr))
+  let has = true
+
+  arr.forEach(x => {
+    if (!set.has(x)) has = false
+  })
+  return has
 }
